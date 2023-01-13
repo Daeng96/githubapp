1a90723dcaf7e91d86351994e945b57b9a07d042
AKBAR <53459333+Daeng96@users.noreply.github.com>
2023-01-14 00:54:39 +0800

    improvements to the search menu

--- app/src/main/java/com/arteneta/githubapp/view/home/GitHubTopBar.kt
+++ app/src/main/java/com/arteneta/githubapp/view/home/GitHubTopBar.kt
@@ -1,8 +1,13 @@
 package com.arteneta.githubapp.view.home
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -13,20 +18,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import com.arteneta.githubapp.R
-import com.arteneta.githubapp.theme.TopShape
 import com.arteneta.githubapp.theme.Typography
 import com.arteneta.githubapp.view.navigation.NavRoute
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class,
+@OptIn(
+	ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class,
 	ExperimentalComposeUiApi::class
 )
 @Composable
@@ -57,14 +66,17 @@ fun GitHubTopBar(
 									}
 								}
 								false -> {
-									Text(text = stringResource(id = R.string.app_name), color = MaterialTheme.colorScheme.primary)
+									Text(
+										text = stringResource(id = R.string.app_name),
+										color = MaterialTheme.colorScheme.primary
+									)
 								}
 							}
 						}
 					},
 					actions = {
-						if (!isSearch) {
-							IconButton(onClick = { isSearch = true }) {
+						AnimatedVisibility(visible = !isSearch) {
+							IconButton(onClick = { isSearch = true; keyboard?.show() }) {
 								Icon(
 									imageVector = Icons.Outlined.Search,
 									contentDescription = "Search"
@@ -130,40 +142,60 @@ fun GitHubTopBar(
 	}
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchView(
 	userName: String,
 	setUserName: (String) -> Unit,
-	onSearch: () -> Unit
+	searchAction: () -> Unit
 ) {
-	TextField(
-		value = userName,
-		onValueChange = setUserName,
-		shape = TopShape,
-		modifier = Modifier.heightIn(max = 50.dp),
-		label = { Text(text = stringResource(id = R.string.search_hint))},
-		textStyle = Typography.titleMedium,
-		leadingIcon = { Icon(
-			imageVector = Icons.Outlined.Search,
-			contentDescription = "Search Icon"
-		)},
-		trailingIcon = {
-				IconButton(
-					onClick = { setUserName("") },
-					enabled = userName.isNotEmpty()) {
-					Icon(
-						imageVector = Icons.Outlined.Close,
-						contentDescription = "Delete"
-					)
-				}
-		},
+
+	BasicTextField(
+		modifier = Modifier
+			.fillMaxWidth()
+			.background(
+				color = MaterialTheme.colorScheme.primary.copy(0.1f),
+				shape = CircleShape
+			)
+			.border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
+			.padding(start = 16.dp),
+		value = userName, onValueChange = setUserName,
 		keyboardActions = KeyboardActions(
-			onSearch = { onSearch() }
+			onSearch = {
+				defaultKeyboardAction(ImeAction.Search)
+				searchAction()
+			}
 		),
 		keyboardOptions = KeyboardOptions.Default.copy(
 			imeAction = ImeAction.Search,
 			keyboardType = KeyboardType.Text
+		), cursorBrush = Brush.verticalGradient(
+			listOf(
+				MaterialTheme.colorScheme.primary,
+				MaterialTheme.colorScheme.primary.copy(0.2f)
+			)
+		),
+		decorationBox = { innerTextField ->
+			Row(
+				modifier = Modifier.fillMaxWidth(),
+				verticalAlignment = Alignment.CenterVertically,
+				horizontalArrangement = Arrangement.SpaceBetween
+			) {
+				innerTextField()
+				IconButton(
+					onClick = { setUserName("") },
+					enabled = userName.isNotEmpty(),
+					colors = IconButtonDefaults.iconButtonColors(
+						contentColor = Color.Red
+					)
+				) {
+					Icon(Icons.Outlined.Close, contentDescription = "Delete")
+				}
+			}
+		},
+		maxLines = 1,
+		textStyle = Typography.titleMedium.copy(
+			color = MaterialTheme.colorScheme.onBackground,
+			fontFamily = FontFamily.Monospace
 		)
 	)
 }
