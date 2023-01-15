6361fbf83e7c9e0ba7fdffe132d2faa0290987a4
AKBAR <53459333+Daeng96@users.noreply.github.com>
2023-01-15 22:32:56 +0800

    Delete favorite items button added.

--- app/src/main/java/com/arteneta/githubapp/view/FavoriteScreen.kt
+++ app/src/main/java/com/arteneta/githubapp/view/FavoriteScreen.kt
@@ -4,8 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -25,12 +31,22 @@ fun ListFavoritesScreen(
 		contentPadding = PaddingValues(all = 16.dp),
 		verticalArrangement = Arrangement.spacedBy(8.dp),
 		content = {
-		items(items = favorites) { user ->
-			ListItem(
-				login = user.login,
-				avatarUrl = user.avatarUrl,
-				subTitle = stringResource(id = R.string.repo).plus(" ${user.publicRepos}"),
-				itemOnClick = showBottomSheet)
+			items(items = favorites) { user ->
+				ListItem(
+					login = user.login,
+					avatarUrl = user.avatarUrl,
+					subTitle = stringResource(id = R.string.repo).plus(" ${user.publicRepos}"),
+					itemOnClick = showBottomSheet,
+					actionContainer = {
+						IconButton(
+							modifier = Modifier.align(Alignment.CenterEnd), onClick = {
+								detailUsersViewModel.unFavorite(user)
+							}) {
+							Icon(imageVector = Icons.Outlined.Delete, contentDescription = "delete")
+						}
+					}
+				)
+			}
 		}
-	})
+	)
 }
--- app/src/main/java/com/arteneta/githubapp/view/tools/ListItem.kt
+++ app/src/main/java/com/arteneta/githubapp/view/tools/ListItem.kt
@@ -1,48 +1,65 @@
 package com.arteneta.githubapp.view.tools
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterStart
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ListItem(
+fun LazyItemScope.ListItem(
 	login: String,
 	avatarUrl: String?,
 	subTitle: String? = null,
+	actionContainer: @Composable BoxScope.() -> Unit = {},
 	itemOnClick: (String) -> Unit = {}
 ) {
-	Row(
+
+	Box(
 		modifier = Modifier
 			.fillMaxWidth()
-			.clickable { itemOnClick(login) },
-		horizontalArrangement = Arrangement.spacedBy(8.dp),
-		verticalAlignment = CenterVertically
+			.animateItemPlacement(),
 	) {
-		AsyncImage(
-			model = avatarUrl,
-			contentDescription = login,
+		Row(
 			modifier = Modifier
-				.size(50.dp)
-				.clip(CircleShape)
-		)
-		Column(
-			verticalArrangement = Arrangement.spacedBy(8.dp)
+				.align(CenterStart)
+				.fillMaxWidth()
+				.clickable { itemOnClick(login) },
+
+			horizontalArrangement = Arrangement.spacedBy(8.dp),
+			verticalAlignment = CenterVertically
 		) {
-			Text(text = login)
-			subTitle?.let {
-				Text(text = it)
-			}
-			subTitle?.let {
-				Divider()
+			AsyncImage(
+				model = avatarUrl,
+				contentDescription = login,
+				modifier = Modifier
+					.size(50.dp)
+					.clip(CircleShape)
+			)
+			Column(
+				verticalArrangement = Arrangement.spacedBy(8.dp)
+			) {
+				Text(text = login)
+				subTitle?.let {
+					Text(text = it)
+				}
+				subTitle?.let {
+					Divider()
+				}
 			}
 		}
+
+		actionContainer()
+
 	}
 }
